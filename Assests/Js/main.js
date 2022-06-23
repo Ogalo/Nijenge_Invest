7862a701fe41d1ad479107ad1d4d44653964fead
Otieno Harrison Omondi <ogaloharrison@gmail.com>
2022-06-23 13:20:03 +0300

    proposal table styled

---
+++ Assests/Js/main.js
@@ -0,0 +1,9 @@
+let mylist = ["Real state", "Automotive", "Agriculture", "Education", "Logistics"]
+
+
+
+
+function proposalCategory (){
+  document.getElementById("optionSelect").value = mylist.option[mylist.selectedIndex].text;
+
+}
