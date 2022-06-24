b38def26f7e54b15c3970a8eb2e706a861971de1
Otieno Harrison Omondi <ogaloharrison@gmail.com>
2022-06-25 00:50:57 +0300

    Sign Up pages linked and pops up on click

--- Assests/Js/main.js
+++ Assests/Js/main.js
@@ -1,78 +1,31 @@
-// const buttSign = document.querySelectorAll("button")
-// const signUpPage = document.querySelectorAll(".page")
+const toggleSign = document.getElementById("butt1")
+const signUpPage = document.getElementById("signUp")
+const mainPage = document.getElementsByClassName("mainPage")
+
+const closeTab = document.querySelector(".closeBtn")
 
 
-// function loadSignUp (e){
-//   e.preventDefault();
-  
-//   if (buttSign.click == true) {
-//       signUpPage.style.display = "block"
-//   } else {
-//       signUpPage.style.display = "none"
-//   }
-  
-  
-// }
-
-// buttSign.addEventListener("click", loadSignUp);
-
-
-// let selectedPageBtn = "";
-
-// for (let i = 0; i<pages.length; i++){
-//   pages[i].addEventListener("click", function (e){
-//     e.preventDefault();
-//     for(let i =0; i<pages.length; i++){
-//       pages[i].style.display = "none";
-//     }
-//     pages[i].style.display = "block";
-//     selectedPageBtn = i;
-    
-//   })
-// }
-
-const links = document.querySelectorAll('#butt1');
-const pages = document.querySelectorAll('.page');
-const allBtns = document.querySelectorAll('.close-btn');
-console.log(links)
-
-function loadSignUp (e){
+toggleSign.addEventListener("click", (e) =>{
   e.preventDefault();
-}
-  
-function regiForm (){
-  if (links.click == true) {
-    pages.style.display = "block"
-  } 
-  else {
-    pages.style.display = "none"
-}
+  if(signUpPage.style.display === "none"){
+    
+    signUpPage.style.display = "block";
+    
+  }else{
+    
+    signUpPage.style.display = "none";
+    
 
-}
-
-links.addEventListener("click", e =>{
-  console.log(e.target)
+  }
 })
 
-
+closeTab.addEventListener ("click", () => {
+  if(closeTab.parentElement.style.display === "none"){
+    closeTab.parentElement.style.display = "block";
+  }else{
+    closeTab.parentElement.style.display = "none";
+    mainPage.style.display = "none"
+  }
   
-
-// links.addEventListener('click', function(event){
-//         event.preventDefault();
-//         console.log()
-        
-//        if(pages.style.display == "none"){
-//         pages.style.display = "block";
-//        }
-//        else{
-//         pages.style.display = "none";
-//        }
-        
-        
-//     });
-    
-
-// allBtns.addEventListener('click', function(){
-//         return allBtns.parentElement.style.display = "none";
-        
-//     });
+  
+})
